9df8b86ec2eb693d0a7d1e34674bd83a833c8d85
nitishsingh10 <nitishsingh9hfhs@gmail.com>
2026-03-31 10:53:34 +0530

    3rd session

--- letvarconst.js
+++ letvarconst.js
@@ -1,7 +1,20 @@
-let btns = document.getElementById("buttons");
-let text = document.getElementById("color")
+// let btns = document.getElementById("buttons");
+// let text = document.getElementById("color");
 
-btns.addEventListener('click',(e)=>{
-    text.style.color = e.target.textContent;
+// btns.addEventListener('mouseover',(e)=>{
 
+//     text.style.color = e.target.textContent;
+
+// })
+
+document.getElementById('image').addEventListener('click',(e)=>{
+
+    let xAxis = e.x >= 160 && e.x <= 190;
+    let yAxis = e.y >= 127 && e.y <= 158;
+
+    if(yAxis && xAxis){
+        document.getElementById('price').innerHTML = `Price is &#8377 2000`
+    }
+    console.log(e);
+    
 })
